f206031f4875d00cdf5fa0994aab8f4a2c319f81
Paul Hondola <paulhondola@gmail.com>
2024-04-19 15:42:15 +0300

    updated photo

--- COE-Convert/convert_jpg_to_coe.m
+++ COE-Convert/convert_jpg_to_coe.m
@@ -1,22 +1,20 @@
-img = imread('photo.jpeg');
+img = imread('photo.jpg');
 img = imresize(img, [240, 320]); % Resize image if necessary
 
 img = uint8(double(img) * 15 / 255); % Scale from 0-255 to 0-15
 
 fileID = fopen('intermediate_image_data.coe', 'w');
-fprintf(fileID, 'memory_initialization_radix=16;\nmemory_initialization_vector=\n');
+% fprintf(fileID, 'memory_initialization_radix=16;\nmemory_initialization_vector=\n');
+
+i = 0;
 
 for row = 1:size(img, 1)
     for col = 1:size(img, 2)
         r = img(row, col, 1);
         g = img(row, col, 2);
         b = img(row, col, 3);
-        fprintf(fileID, '%01X%01X%01X', r, g, b);
-        if row == size(img, 1) && col == size(img, 2)
-            fprintf(fileID, ';');
-        else
-            fprintf(fileID, ',\n');
-        end
+        fprintf(fileID, '%01X%01X%01X ', r, g, b);
     end
 end
+
 fclose(fileID);
